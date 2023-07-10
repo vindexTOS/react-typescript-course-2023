f9033fe484b694863db9feba9e7506c1f5a2cfa8
VindexTOS <giorgikutateladze1998@gmail.com>
2023-07-10 23:07:00 +0400

    use reducer training finnished

--- src/training-field/3-user-reducer/final/1-use-reducer-final.tsx
+++ src/training-field/3-user-reducer/final/1-use-reducer-final.tsx
@@ -1,13 +1,26 @@
-import React, { Dispatch, useReducer } from 'react'
+import React, { Dispatch, useEffect, useReducer } from 'react'
+
+type UserType = {
+  name: string
+  age: number
+}[]
 
 type State = {
   num: number
+  user: UserType
 }
 
 type Action = {
-  type: 'increase' | 'decrease' | 'add'
-  payload?: number
+  type: 'increase' | 'decrease' | 'add' | 'user'
+
+  payload?: number | UserType
 }
+
+const userData = [
+  { name: 'gio', age: 23 },
+  { name: 'dato', age: 32 },
+  { name: 'chainika', age: 66 },
+]
 
 const Usereducerfinal = () => {
   const reducer = (state: State, action: Action): State => {
@@ -17,14 +30,21 @@ const Usereducerfinal = () => {
       case 'decrease':
         return { ...state, num: state.num - 1 }
       case 'add':
-        return { ...state, num: state.num = action.payload || 0 }
+        return { ...state, num: (action.payload as number) || 0 }
+      case 'user':
+        return { ...state, user: action.payload as UserType }
       default:
         return state
     }
   }
 
+  useEffect(() => {
+    dispatch({ type: 'user', payload: userData })
+  }, [])
+
   const [state, dispatch] = useReducer(reducer, {
     num: 0,
+    user: [],
   })
 
   return (
@@ -37,6 +57,18 @@ const Usereducerfinal = () => {
         }
       />
       <p>{state.num}</p>
+
+      <div>
+        {/* დავალება, make val UserType  */}
+        {state.user?.map((val: any) => {
+          return (
+            <div key={val.name}>
+              <h1>{val.name}</h1>
+              <h1>{val.age}</h1>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
