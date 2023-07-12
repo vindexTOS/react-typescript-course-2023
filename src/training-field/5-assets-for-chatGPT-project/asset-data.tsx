fbf4f5faa1e719cbc5a2a1c95f398db81d5d4402
VindexTOS <giorgikutateladze1998@gmail.com>
2023-07-12 23:39:33 +0400

    API link added

--- src/training-field/5-assets-for-chatGPT-project/asset-data.tsx
+++ src/training-field/5-assets-for-chatGPT-project/asset-data.tsx
@@ -1,4 +1,5 @@
 export const assetData = {
   userPhoto: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
   botPhoto: 'https://cdn-icons-png.flaticon.com/512/4712/4712139.png',
+  quoteAPI: 'https://type.fit/api/quotes',
 }
